245bf317a899af85aec46108e6d9be152da32476
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-05-23 19:24:16 +0530

    JavascriptEvents

---
+++ SAI FullStack classes/javascript-classes/myown/22-05-2023/events/index.js
@@ -0,0 +1,11 @@
+let a = document.querySelector("p");
+let b = document.querySelector("button");
+
+//function1 chnage color
+
+b.addEventListener("click", function () {
+  a.style.color = "red";
+});
+b.addEventListener("click", function () {
+  a.style.fontSize = "2em";
+});
